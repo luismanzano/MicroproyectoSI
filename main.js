eb7ff3dae17857cda8e269d00696d364c419677d
Luis Fernando <luis.manzano@correo.unimet.edu.ve>
2020-02-07 22:50:59 -0400

    LISTOOOOOOOO

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 document.getElementById("send").onclick = function(){
-    alert("Se ha enviado el formulario, muchas gracias por contactarme!")
+    
     var nombre = "luis";
 
+    if(document.getElementById("name").value !="" && document.getElementById("email").value != "" && document.getElementById("msg").value!="" && document.getElementById("phone").value!="") {
     console.log("Su nombre es: " + document.getElementById("name").value);
+    console.log("Su telefono es: " + document.getElementById("phone").value);
     console.log("Su email es: " + document.getElementById("email").value);
     console.log("Su mensaje es: " + document.getElementById("msg").value);
+    alert("Se ha enviado el formulario, muchas gracias por contactarme!")
+
+    } else{
+        alert("Debe rellenar todos los elementos del formulario")
+    }
 }
